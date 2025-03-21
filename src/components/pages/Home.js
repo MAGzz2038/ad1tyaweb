9f215d1e24c718015b337b4045e92a21fe96f12c
Anmol Manked <79021740+MAGzz2038@users.noreply.github.com>
2025-03-22 01:18:16 +0530

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Pages.css';
+import Navbar from './components/Navbar';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
 import Bottompage from '../Bottompage';
@@ -7,6 +8,7 @@ import Bottompage from '../Bottompage';
 function Home() {
   return (
     <>
+      <Navbar/>
       <HeroSection />
       <Cards/>
       <Bottompage/>
